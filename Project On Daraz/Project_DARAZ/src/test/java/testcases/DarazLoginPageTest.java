0fa08a29643d80c55fb8e4c46718071b39d7f64b
KishoarJahanTithee <77774039+KishoarJahanTithee@users.noreply.github.com>
2023-05-01 01:03:24 +0600

    Add files via upload
    Final Project Uploaded

---
+++ Project On Daraz/Project_DARAZ/src/test/java/testcases/DarazLoginPageTest.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import pages.DarazHomePage;
+import pages.DarazLoginPage;
+import utilities.BaseDriverSetup;
+import utilities.DataSet;
+
+public class DarazLoginPageTest extends BaseDriverSetup{
+	DarazHomePage darazHomePage = new DarazHomePage();
+	DarazLoginPage darazLoginPage = new DarazLoginPage();
+	
+	
+	@Test(dataProvider = "invalidCredentials", dataProviderClass = DataSet.class)
+	public void testLoginwithInvalidPhoneNumber(String username, String password, String ErrorMessage) {
+		getDriver().get(darazHomePage.HOMEPAGE_URL);
+		darazHomePage.clickOnElement(darazHomePage.SIGNUP_LOGIN_BUTTON);
+		darazLoginPage.wirteTex(darazLoginPage.PHONE_EMAIL_INPUT_FIELD, username);
+		darazLoginPage.wirteTex(darazLoginPage.PASSWORD_INPUT_FIELD, password);
+		darazLoginPage.takeScreenShot("Login Info");
+		darazLoginPage.clickOnElement(darazLoginPage.LOGIN_BUTTON);
+		darazLoginPage.takeScreenShot("Login");
+		assertEquals(darazLoginPage.getElement(darazLoginPage.ERROR_MESSAGE).getText(), ErrorMessage);  
+	}
+}
